f8043c3b3eca94c34a9fc1c13e06987f92fc2882
R.J.V. Bertin <rjvbertin@gmail.com>
2018-03-07 18:21:26 +0100

    revert the previous commit
    Committed from host : Portia.local

--- src/qcocoa-qpa/qcocoawindow.mm
+++ src/qcocoa-qpa/qcocoawindow.mm
@@ -46,7 +46,6 @@
 #include "qcocoahelpers.h"
 #include "qcocoanativeinterface.h"
 #include "qnsview.h"
-#include "qcocoamenuloader.h"
 #include <QtCore/qfileinfo.h>
 #include <QtCore/private/qcore_mac_p.h>
 #include <qwindow.h>
@@ -1148,12 +1147,7 @@ void QCocoaWindow::setWindowTitle(const QString &title)
         return;
 
     CFStringRef windowTitle = title.toCFString();
-    NSString *nsTitle = const_cast<NSString *>(reinterpret_cast<const NSString *>(windowTitle));
-    [m_nsWindow setTitle: nsTitle];
-//     if (![NSApp windowsMenu]) {
-//         [NSApp setWindowsMenu:[[QCocoaMenuLoader sharedMenuLoader] applicationMenu]];
-//     }
-//     [NSApp changeWindowsItem:m_nsWindow title:nsTitle filename:NO];
+    [m_nsWindow setTitle: const_cast<NSString *>(reinterpret_cast<const NSString *>(windowTitle))];
     CFRelease(windowTitle);
 }
 
